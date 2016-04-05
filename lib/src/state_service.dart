b180527c4145f09b97551bb244c69845ced7d697
Frank Pepermans <frank@igindo.com>
2016-04-05 11:21:16 +0200

    no message

--- lib/src/state_service.dart
+++ lib/src/state_service.dart
@@ -179,7 +179,9 @@ class StateService {
 
         existing = _factory.spawn(result, _serializer, (Entity serverEntity, Entity clientEntity) => ConflictManager.AcceptClient);
 
-        _snapshot = (existing as List<StateContainer>).map((StateContainer container) => container.duplicate());
+        _snapshot = (existing as List<StateContainer>)
+          .map((StateContainer container) => container.duplicate())
+          .toList(growable: false);
       } catch (error) {
         exceptionHandler.call(error, error.stackTrace, 'Failed to reopen last state: $existingState');
       }
